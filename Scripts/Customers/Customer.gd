97a4a0ad1139af4c9f0a7f80a9b4a34ddf67db90
uGustaHd <154080279+uGustaHd@users.noreply.github.com>
2026-01-16 20:24:08 -0300

    Create customer.gd with setup logic

--- Scripts/Customers/Customer.gd
+++ Scripts/Customers/Customer.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
-@export var CustomerData: CustomerData
+@export var customer_data: CustomerData
 var current_request: Dictionary
 
+func setup():
+	if customer_data != null:
+		$Sprite2D.texture = customer_data.sprite
+		current_request = customer_data.possible_requests.pick_random()
+	else:
+		print("No CustomerData")
+
 func _ready() -> void:
-	$Sprite2D.texture = CustomerData.sprite
+	setup()
